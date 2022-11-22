6a224042cf0a59024324bf78d86307c692dab94f
RomPop76 <r.popielas@gmail.com>
2022-11-22 06:15:34 +0100

    Commit 2

--- colors.js
+++ colors.js
@@ -1,2 +1,14 @@
+const orangeBtn = document.querySelector('.orange');
+const greenBtn = document.querySelector('.green');
+const blueBtn = document.querySelector('.blue');
+const yellowBtn = document.querySelector('.yellow');
+const redBtn = document.querySelector('.red');
 
- 
+orangeBtn.addEventListener("click", function() {
+  // document.body.className = "orange";
+  document.body.classList.add('orange');
+})
+
+greenBtn.addEventListener("click", function() {
+  document.body.classList.add(orange);
+})

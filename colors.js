32e9fa9f7a62eaf7c0494489e82501d5674fde03
RomPop76 <r.popielas@gmail.com>
2022-11-22 22:36:27 +0100

    Commit 3

--- colors.js
+++ colors.js
@@ -1,14 +1,26 @@
-const orangeBtn = document.querySelector('.orange');
-const greenBtn = document.querySelector('.green');
-const blueBtn = document.querySelector('.blue');
-const yellowBtn = document.querySelector('.yellow');
-const redBtn = document.querySelector('.red');
+// const orangeBtn = document.querySelector('.orange');
+// const greenBtn = document.querySelector('.green');
+// const blueBtn = document.querySelector('.blue');
+// const yellowBtn = document.querySelector('.yellow');
+// const redBtn = document.querySelector('.red');
 
-orangeBtn.addEventListener("click", function() {
-  // document.body.className = "orange";
-  document.body.classList.add('orange');
+const items = document.querySelectorAll('div');
+items.forEach(function(item){
+  item.addEventListener('click', changeColor)
 })
 
-greenBtn.addEventListener("click", function() {
-  document.body.classList.add(orange);
-})
+function changeColor() {
+  document.body.className = this.className;
+}
+
+
+// orangeBtn.addEventListener("click", changeColor);
+// greenBtn.addEventListener("click", changeColor);
+// blueBtn.addEventListener("click", changeColor);
+// yellowBtn.addEventListener("click", changeColor);
+// redBtn.addEventListener("click", changeColor);
+
+// greenBtn.addEventListener("click", function() {
+//   document.body.className = "green";
+//   // document.body.classList.toggle('green');
+// })
